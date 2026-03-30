0cb788089c0bd0156725a49d915fd499851bbcc4
Mohamed Amine Larbi <larbi.mohamedamine@esprit.tn>
2026-03-30 22:59:20 +0100

    Add initial setup for Reclamation Reponse Microservice
    - Create Maven wrapper files for project build management.
    - Add .gitignore to exclude build artifacts and IDE-specific files.
    - Set up .gitattributes for line endings.
    - Initialize pom.xml with Spring Boot dependencies and configurations.
    - Implement main application class and test class for context loading.

---
+++ ReclamationReponseMicroservice/src/test/java/tn/esprit/reclamationreponsemicroservice/ReclamationReponseMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.reclamationreponsemicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReclamationReponseMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
